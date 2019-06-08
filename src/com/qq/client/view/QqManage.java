4b2e8f0c0dcb06bf44b2439131521816d8c5d94f
Aurora <2842240341@qq.com>
2019-06-08 12:47:52 +0800

    添加了搜索和管理界面类

---
+++ src/com/qq/client/view/QqManage.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: QqManage
+ * Author:   trhjyhf
+ * Date:     2019/6/8 12:47
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.qq.client.view;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author trhjyhf
+ * @create 2019/6/8
+ * @since 1.0.0
+ */
+public class QqManage {
+
+}
